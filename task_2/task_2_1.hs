f346e3eae0b48be157a9d2f6a72c8196769ff86d
maximpopsuyko <maxim.popsuyko@gmail.com>
2018-01-18 17:09:20 +0300

    Haskell add task_2

---
+++ task_2/task_2_1.hs
@@ -0,0 +1,67 @@
+--Реализуйте структуру данных "бинарное дерево поиска".
+
+data BinaryTree = EmptyTree | LeafTree Integer | Node Integer BinaryTree BinaryTree deriving (Show)
+
+-- BinaryTree - пустое дерево
+-- LeafTree Integer - лист дерева
+-- Node Integer BinaryTree BinaryTree - узел, включающий левое и правое дерево
+
+-- Добавления элемента:
+insert :: BinaryTree -> Integer -> BinaryTree
+insert EmptyTree x = LeafTree x
+insert (LeafTree value) x | x < value = Node value (LeafTree x) EmptyTree
+                       | x > value = Node value EmptyTree (LeafTree x)
+                       | otherwise = LeafTree value
+insert (Node value leftTree rightTree) x | x < value = Node value (insert leftTree x) rightTree
+                                         | x > value = Node value leftTree (insert rightTree x)
+                                         | otherwise = Node value leftTree rightTree
+
+--Удаления элемента: 
+remove :: BinaryTree -> Integer -> BinaryTree
+remove EmptyTree     _ = EmptyTree
+remove (LeafTree value) x | value == x    = EmptyTree        
+                       | otherwise     = LeafTree value 
+remove (Node value leftTree rightTree) x | x < value = Node value (remove leftTree x) rightTree
+                                         | x > value = Node value leftTree (remove rightTree x)
+                                         | otherwise = concatTree leftTree rightTree
+                                         where concatTree EmptyTree    tree = tree
+                                               concatTree (LeafTree v)    tree = Node v EmptyTree tree
+                                               concatTree (Node v l r) tree = Node value l (concatTree r tree)
+
+-- Создания пустого дерева:
+emptyTree :: BinaryTree
+emptyTree = EmptyTree
+
+-- Поиска элемента в дереве:
+containsElement :: BinaryTree -> Integer -> Bool
+containsElement EmptyTree                       _ = False
+containsElement (LeafTree value)                   x = x == value
+containsElement (Node value leftTree rightTree) x | x < value = containsElement leftTree x
+                                                  | x > value = containsElement rightTree x
+                                                  | otherwise = True
+
+-- Поиска в дереве наименьшего элемента, который больше или равен заданному:
+nearestGE :: BinaryTree -> Integer -> Integer
+nearestGE EmptyTree x = undefined
+nearestGE (LeafTree value) x | x == value = value
+                          | otherwise = undefined
+nearestGE (Node value leftTree@(EmptyTree) rightTree) x | x <= value = value
+                                                        | otherwise = nearestGE rightTree x
+nearestGE (Node value leftTree@(LeafTree leftValue) rightTree) x | x == value = value
+                                                              | x < value = if x > leftValue then value
+                                                                                             else nearestGE leftTree x
+                                                              | otherwise = nearestGE rightTree x
+nearestGE (Node value leftTree@(Node leftValue leftleftTree leftrightTree) rightTree) x | x == value = value
+                                                                                        | x < value = if x > leftValue then value
+																						              else nearestGE leftTree x
+                                                                                        | otherwise = nearestGE rightTree x
+
+-- Создания дерева из списка:
+treeFromList :: [Integer] -> BinaryTree
+treeFromList list = foldl insert EmptyTree list
+
+-- Создания списка из дерева:
+listFromTree :: BinaryTree -> [Integer]
+listFromTree EmptyTree            = []
+listFromTree (LeafTree value)        = [value]
+listFromTree (Node value leftTree rightTree) = [value] ++ (listFromTree leftTree) ++ (listFromTree rightTree)
---
+++ task_2/task_2_2.hs
@@ -0,0 +1,64 @@
+--foldl
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' f x [] = x
+foldl' f x (head:list) = foldl f (f x head) list
+
+--foldr
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f x [] = x
+foldr' f x (head:list) = f head (foldr f x list)
+
+--map
+map' :: (a -> b) -> [a] -> [b]
+map' f a = foldr' (\n list -> (f n) : list) [] a
+
+--flatMap
+flatMap' :: (a -> [b]) -> [a] -> [b]
+flatMap' f a = foldr (\n list -> (f n) ++ list) [] a
+
+--concat
+concat' :: [a] -> [a] -> [a]
+concat' a b = foldl' (\list n -> list ++ [n]) a b
+
+--filter
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f a = foldl' (\list n -> list ++ (if f n then [n] else [])) [] a
+
+--maxBy
+maxBy' :: (a -> Integer) -> [a] -> a
+maxBy' f a@(n:list) = foldl' (\x y -> if f x > f y then x else y) n a
+ 
+--minBy
+minBy' :: (a -> Integer) -> [a] -> a
+minBy' f a@(n:list) = foldl' (\x y -> if f x < f y then x else y) n a
+
+--reverse
+reverse' :: [a] -> [a]
+reverse' a = foldl' (\list n -> n : list) [] a
+
+--elementAt 
+elementAt' :: Integer -> [a] -> a
+elementAt' index (head:list) = snd
+                                (
+								    foldl'
+									    (
+										    \(i, x) y -> if i < 0
+                                                         then (i-1, x)
+                                                         else (i-1, y)
+										)
+                                        (index-1, head)
+                                        list
+								)
+								
+--indexOf
+indexOf' :: String -> [String] -> Integer
+indexOf' str list = if snd y then fst y else error "not found" where
+                                y = foldl'
+                                    (\x n -> if snd x == False
+                                        then if str == n
+                                            then (fst x, True)
+                                            else (fst x + 1, False)
+                                        else x
+                                    )
+                                    (0, False)
+                                    list
