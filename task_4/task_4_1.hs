ac8a4fc0394c1f30a150e9c98e24d5099d659918
maximpopsuyko <maxim.popsuyko@gmail.com>
2018-01-22 01:45:44 +0300

    Update task_4_1.hs

--- task_4/task_4_1.hs
+++ task_4/task_4_1.hs
@@ -4,10 +4,10 @@ instance Functor FunMonad where
     fmap f (FunMonad x) = FunMonad (\() -> f (x ()) )
 
 instance Applicative FunMonad where
-    pure x = FunMonad $ \() -> x
+    pure x                    = FunMonad $ \() -> x
     FunMonad a <*> FunMonad b = FunMonad $ \() -> (a ()) (b ())
 
 instance Monad FunMonad where
     return a  = FunMonad ( \() -> a )
-    m >>= k = k (fun m ())  
-    fail = error
+    m >>= k   = k (fun m ())  
+    fail      = error
