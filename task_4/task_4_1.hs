f3dd85e7dc9ca52337b520b054a0c04bdcf592ec
maximpopsuyko <maxim.popsuyko@gmail.com>
2018-01-22 01:43:55 +0300

    add task_4

---
+++ task_4/task_4_1.hs
@@ -0,0 +1,13 @@
+data FunMonad a = FunMonad { fun :: () -> a }    
+
+instance Functor FunMonad where
+    fmap f (FunMonad x) = FunMonad (\() -> f (x ()) )
+
+instance Applicative FunMonad where
+    pure x = FunMonad $ \() -> x
+    FunMonad a <*> FunMonad b = FunMonad $ \() -> (a ()) (b ())
+
+instance Monad FunMonad where
+    return a  = FunMonad ( \() -> a )
+    m >>= k = k (fun m ())  
+    fail = error
